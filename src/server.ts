eca91af25d8828949cbbf89b04f52493f3d1a333
sadmanalam142 <sadmanalam142@gmail.com>
2024-01-25 01:47:26 +0600

    testing eslint

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "../src/app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from '../src/app'
+import config from './config/index'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log('Database Connected Successfull');
+    await mongoose.connect(config.database_url as string)
+    console.log('Database Connected Successfull')
 
     app.listen(config.port, () => {
-        console.log(`Application listening on port ${config.port}`)
-      })
+      console.log(`Application listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("Failed to connect database", error);
+    console.log('Failed to connect database', error)
   }
 }
 
