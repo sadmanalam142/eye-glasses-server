1e497268136fd33f76f37140414b83017fb4d524
sadmanalam142 <sadmanalam142@gmail.com>
2024-01-25 02:25:17 +0600

    esling working successfully

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: any, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('This is root route')
 })
 
